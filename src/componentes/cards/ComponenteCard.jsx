4a84d35bf9de4401ff00dd3268b8341351927bd6
Martin Bruno <mbruno@desarrollosocial.gob.ar>
2024-11-28 15:16:21 -0300

    ultimos detalles

--- src/componentes/cards/ComponenteCard.jsx
+++ src/componentes/cards/ComponenteCard.jsx
@@ -3,7 +3,7 @@ import './ComponenteCard.css'
 
 export default function ComponenteCard({ componente }) {
   return (
-    <div className='container'>
+    <div className='container-componente'>
       <div className='container-banda'></div>
       <div className='container-info'>
         <h5>{componente.nombre}</h5>
